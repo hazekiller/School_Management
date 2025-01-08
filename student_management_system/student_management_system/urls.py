4b9ad9e05b4aa98a536787411d40482c6f47d5bd
hazekiller <prakashtimilsina76@gmail.com>
2025-01-08 17:30:54 +0545

    added different views to main projet

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views, Hod_Views, Student_Views, Staff_Views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
